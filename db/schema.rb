543a225a970dce7efdb2d328fa5a5e6061528be0
Samantha Kreisel <samikreisel@gmail.com>
2023-12-29 18:02:21 -0600

    Notes WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_26_214525) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_29_233640) do
   create_table "campaigns", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
